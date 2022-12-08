bec55b4c0bad2c51912b6caeb508e4d6461ce36e
Daniel Apostoli <danielgapostoli@gmail.com>
2022-12-08 12:12:48 -0600

    Added exercises

---
+++ Data Structures Final/BlockChain/BlockChain.py
@@ -0,0 +1,28 @@
+import hashlib
+
+class Node:
+    def __init__(self, block):
+        self.block = block
+        self.next = None
+
+class Block:
+    def __init__(self, timestamp, data, previous_hash):
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash()
+
+    def calc_hash(self):
+        sha = hashlib.sha256()
+        hash_str = self.data.encode('utf-8')
+        sha.update(hash_str)
+        return sha.hexdigest()
+
+# Add your own test cases: include at least three test cases
+# and two of them must include edge cases, such as null, empty or very large values
+
+# Test Case 1
+
+# Test Case 2
+
+# Test Case 3
