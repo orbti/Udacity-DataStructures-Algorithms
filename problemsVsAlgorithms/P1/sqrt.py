4a87d1c0eea0c5efa5cf5bd42a2d3b1345514807
Daniel Apostoli <danielgapostoli@gmail.com>
2023-01-14 12:25:19 -0600

    sqrt update

--- problemsVsAlgorithms/P1/sqrt.py
+++ problemsVsAlgorithms/P1/sqrt.py
@@ -15,30 +15,33 @@ Here is some boilerplate code and test cases to start with:
 """
 
 def sqrt(number):
-    """
-    Calculate the floored square root of a number
+   """
+   Calculate the floored square root of a number
 
-    Args:
-       number(int): Number to find the floored squared root
-    Returns:
-       int: Floored Square Root
-    """
-    if number == 1:
+   Args:
+      number(int): Number to find the floored squared root
+   Returns:
+      int: Floored Square Root
+   """
+   if number == 1:
       return 1
-    min = 0
-    max = number
-    mid = (min + max)/2
-    for _ in range(30):
+   min = 0
+   max = number
+   mid = (min + max)/2
+   i = 0
+   while i < number*10:
       if mid * mid == number:
          break
       elif mid*mid > number:
          max = mid
          mid = (min+max)/2
+         i += 1
       else:
          min = mid
          mid = (min+max)/2
-    
-    return int(mid)
+         i += 1
+
+   return int(mid)
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
