4dc9a1d79414884f4d519564c68bee01ec0f3c13
Daniel Apostoli <danielgapostoli@gmail.com>
2023-01-17 13:00:11 -0600

    updated p1

--- problemsVsAlgorithms/P1/sqrt.py
+++ problemsVsAlgorithms/P1/sqrt.py
@@ -14,7 +14,7 @@ The expected time complexity is O(log(n))
 Here is some boilerplate code and test cases to start with:
 """
 
-def sqrt(number):
+def sqrt(number:int) -> int:
    """
    Calculate the floored square root of a number
 
@@ -23,6 +23,12 @@ def sqrt(number):
    Returns:
       int: Floored Square Root
    """
+   if not isinstance(number, int):
+      raise TypeError("must be real number")
+
+   if number < 0:
+      raise ValueError("input must be zero or greater")
+
    if number == 1:
       return 1
    min = 0
@@ -37,12 +43,21 @@ def sqrt(number):
       else:
          min = mid
          mid = (min+max)/2
-
    return int(mid)
+
+def iserror(func, *args, **kw):
+   try:
+      func(*args, **kw)
+      return False
+   except Exception:
+      return True
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print ("Pass" if  (iserror(sqrt,-1)) else "Fail")
+print ("Pass" if  (iserror(sqrt,'abc')) else "Fail")
+print ("Pass" if  (iserror(sqrt,None)) else "Fail")
 
